c7eb46c5ea17a7288f4f5d391b986a5b67c4b846
Asmah Emmanuel <atbhae100@gmail.com>
2022-04-19 07:06:13 -0700

    crud

---
+++ config/dbConnect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/blog");
+        // await mongoose.connect("mongodb://127.0.0.1:27017/blog", {
+        //     dbName: "blog"
+        // });
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+};
+
+module.exports = {
+    dbConnect,
+};
